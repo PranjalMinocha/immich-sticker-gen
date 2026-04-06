ce8257fde7e6f6690059ea80075c14efacec3695
changwonj <richardj0916@gmail.com>
2026-04-06 17:45:41 -0400

    add download url

--- data/dummy_api.py
+++ data/dummy_api.py
@@ -16,7 +16,7 @@ app = FastAPI()
 S3_ENDPOINT = os.environ.get("S3_ENDPOINT")
 S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY")
 S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY")
-RAW_BUCKET = os.environ.get("RAW_BUCKET", "immich-raw-data")
+RAW_BUCKET = os.environ.get("RAW_BUCKET")
 
 s3 = boto3.client(
     's3',
